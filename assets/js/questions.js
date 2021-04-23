e295cfa0fbe73da096f75efa523d9d6cb5876fe8
Miguel Alicea <76752632+malicea0783@users.noreply.github.com>
2021-04-23 16:36:19 -0500

    worked out some functions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,17 @@
-let questions = [{}, {}];
+let questions = [
+  {
+    question: "what is scotss favorite color?",
+    choices: ["blue", "hotpink", "red", "green"],
+    correctAnswer: "hotpink",
+  },
+  {
+    question: "how much is too much?",
+    choices: ["never too much", "much too much", "what?", "two"],
+    correctAnswer: "never too much",
+  },
+  {
+    question: "why?",
+    choices: ["cuz", "because", "why not?", "ok"],
+    correctAnswer: "ok",
+  },
+];
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,105 +1,107 @@
 // define variables /////////////////////////////////////////////////////////////////
 
-let startBtn = document.getElementById('start');
+let startBtn = document.getElementById("start");
 let saveScore = document.getElementById("save-score");
-
-let questions = [
-    {
-        question: 'what is scotss favorite color?',
-        answers: ['blue', 'hotpink', 'red', 'green'],
-        correctAnswer: 'hotpink',
-    },
-    {
-        question: 'how much is too much?',
-        answers: ['never too much', 'much too much', 'what?', 'two'],
-        correctAnswer: 'never too much',
-    },
-    {
-        question: 'why?',
-        answers: ['cuz', 'because', 'why not?', 'ok'],
-        correctAnswer: 'ok',
-    },
-];
-
-let choice1 = document.createElement("button");
-let choice2 = document.createElement("button");
-let choice3 = document.createElement("button");
-let choice4 = document.createElement("button");
-
-document.getElementById("choices").appendChild(choice1);
-document.getElementById("choices").appendChild(choice2);
-document.getElementById("choices").appendChild(choice3);
-document.getElementById("choices").appendChild(choice4);
-
-
-
+let questionTitle = document.getElementById("question-title");
+let choicesDiv = document.getElementById("choices");
+let questionIndex = 0;
+let score = 0;
+let countdown = 125;
 
 // functions ////////////////////////////////////////////////////////////////
 // start quiz init
 function startQuiz() {
-    // start timer
-    // find DOM element to show the question
+  // start timer
+  // find DOM element to show the question
     getQuestion();
-
+    startTimer();
 }
 
 // get the next question
 function getQuestion() {
-    // get the current question
-    // show the question
-    // loop show the buttons (choices)
-        // add event listener for each button created
-    answerCheck();
+  // get the current question
+  let currentQuestion = questions[questionIndex];
+  // show the question
+  questionTitle.textContent = currentQuestion.question;
+  // loop show the buttons (choices)
+  currentQuestion.choices.forEach((choice) => {
+    let choiceBtn = document.createElement("button");
+    choiceBtn.textContent = choice;
+      choiceBtn.setAttribute("value", choice);
+      choiceBtn.setAttribute("class", "option")
+    // add event listener for each button created
+    // choiceBtn.onclick = verifyAnswer;
+    choiceBtn.addEventListener("click", verifyAnswer);
+    choicesDiv.appendChild(choiceBtn);
+  });
+
+  // verifyAnswer();
 }
 
 // check answer selection
-function answerCheck() {
-    // check the user selection against correct answer
-    // incorrect remove seconds
-    // set score
-    // get the next question
-    getQuestion();
-    // if questions.length
-    endGame();
+function verifyAnswer() {
+  if (this.value === questions[questionIndex].correctAnswer) {
 
+      score += 5;
+  } else {
+      countdown -= 10;
+  }
+    
+  questionIndex++;
+  if (questionIndex < questions.length) {
+    showQuestion();
+  }
+  // check the user selection against correct answer
+  // incorrect remove seconds
+  // set score
+  // get next question
+  // showQuestion();
+  // if questions.length
+}
+
+//different fn to change the text of the appended buttons so as to not have more buttons appearing out of nowhere
+function showQuestion() {
+    questionTitle.textContent = questions[questionIndex].question
+    //query selector all is getting an array of all elements with the class option
+    var buttonMatrix = document.querySelectorAll(".option")
+    //forEach loop has an optional set of parameters the i in this case indicates index
+    //element is just the elemnt in the array
+    buttonMatrix.forEach((element, i) => {
+        element.textContent = questions[questionIndex].choices[i]
+    })
+    console.log(buttonMatrix)
+}
+
+//Starts the Timer!!!!
+function startTimer() {
+    var interval = setInterval(function () {
+        countdown--;
+        document.getElementById("timer").textContent = countdown;
+
+        if (countdown <= 0 || questionIndex >= questions.length) {
+            clearInterval(interval);
+            endGame();
+        }
+    }, 1000)
 }
 
 // end game
 function endGame() {
-    // set their score
-    // show end screen
-    // clear out timer
+  alert;
+  // set their score
+  // show end screen
+  // clear out timer
 }
 
 // save high score
 function saveHighScore() {
-    // prompt for initials
-    // save score to localstorage
+  // prompt for initials
+  // save score to localstorage
 }
-
-
-
 
 // event listeners //////////////////////////////////////////
 // start button click
-startBtn.addEventListener('click', startQuiz);
+startBtn.addEventListener("click", startQuiz);
 
 // save high score
-saveScore.addEventListener('click', saveHighScore);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// saveScore.addEventListener('click', saveHighScore);
