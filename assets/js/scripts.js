86ac0fcd6e52bd8ee73fe133c03e878a9424ea98
Miguel Alicea <76752632+malicea0783@users.noreply.github.com>
2021-04-25 02:15:28 -0500

    worked out more javascript and html

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,107 +1,109 @@
-// define variables /////////////////////////////////////////////////////////////////
-
 let startBtn = document.getElementById("start");
-let saveScore = document.getElementById("save-score");
 let questionTitle = document.getElementById("question-title");
 let choicesDiv = document.getElementById("choices");
+let initialInput = document.getElementById("initials");
+let saveScore = document.getElementById("save-score");
+let instructions = document.getElementById('instructions');
+let questionContainer = document.getElementById('question-container');
+let form = document.getElementById('form');
 let questionIndex = 0;
-let score = 0;
 let countdown = 125;
 
-// functions ////////////////////////////////////////////////////////////////
-// start quiz init
-function startQuiz() {
-  // start timer
-  // find DOM element to show the question
-    getQuestion();
-    startTimer();
+function init() {
+ instructions.style.display = 'block';
+  questionContainer.style.display = 'none';
+  form.style.display = 'none';
 }
 
-// get the next question
+function startQuiz() {
+ instructions.style.display = 'none';
+  questionContainer.style.display = 'block';
+  getQuestion();
+  startTimer();
+  if (startBtn.style.display === "none") {
+    startBtn.style.display = "block";
+  } else {
+    startBtn.style.display = "none";
+  }
+}
+
 function getQuestion() {
-  // get the current question
   let currentQuestion = questions[questionIndex];
-  // show the question
   questionTitle.textContent = currentQuestion.question;
-  // loop show the buttons (choices)
   currentQuestion.choices.forEach((choice) => {
     let choiceBtn = document.createElement("button");
     choiceBtn.textContent = choice;
-      choiceBtn.setAttribute("value", choice);
-      choiceBtn.setAttribute("class", "option")
-    // add event listener for each button created
-    // choiceBtn.onclick = verifyAnswer;
+    choiceBtn.setAttribute("value", choice);
+    choiceBtn.setAttribute("class", "option");
     choiceBtn.addEventListener("click", verifyAnswer);
     choicesDiv.appendChild(choiceBtn);
   });
-
-  // verifyAnswer();
 }
 
-// check answer selection
 function verifyAnswer() {
   if (this.value === questions[questionIndex].correctAnswer) {
-
-      score += 5;
   } else {
-      countdown -= 10;
-  }
+    countdown -= 10;
     
+  }
+
   questionIndex++;
   if (questionIndex < questions.length) {
     showQuestion();
   }
-  // check the user selection against correct answer
-  // incorrect remove seconds
-  // set score
-  // get next question
-  // showQuestion();
-  // if questions.length
 }
 
-//different fn to change the text of the appended buttons so as to not have more buttons appearing out of nowhere
 function showQuestion() {
-    questionTitle.textContent = questions[questionIndex].question
-    //query selector all is getting an array of all elements with the class option
-    var buttonMatrix = document.querySelectorAll(".option")
-    //forEach loop has an optional set of parameters the i in this case indicates index
-    //element is just the elemnt in the array
-    buttonMatrix.forEach((element, i) => {
-        element.textContent = questions[questionIndex].choices[i]
-    })
-    console.log(buttonMatrix)
+  questionTitle.textContent = questions[questionIndex].question;
+  var buttonMatrix = document.querySelectorAll(".option");
+  buttonMatrix.forEach((element, i) => {
+    element.textContent = questions[questionIndex].choices[i];
+  });
 }
 
-//Starts the Timer!!!!
 function startTimer() {
-    var interval = setInterval(function () {
-        countdown--;
-        document.getElementById("timer").textContent = countdown;
+  var interval = setInterval(function () {
+    countdown--;
+    document.getElementById("timer").textContent = countdown;
 
-        if (countdown <= 0 || questionIndex >= questions.length) {
-            clearInterval(interval);
-            endGame();
-        }
-    }, 1000)
+    if (countdown <= 0 || questionIndex >= questions.length) {
+      clearInterval(interval);
+      endGame();
+    }
+  }, 1000);
 }
 
 // end game
 function endGame() {
-  alert;
+  alert("GAME OVER MAN! GAME OVER!!!");
+  questionContainer.style.display = 'none';
+  form.style.display = 'block';
   // set their score
   // show end screen
   // clear out timer
 }
 
-// save high score
-function saveHighScore() {
-  // prompt for initials
-  // save score to localstorage
+function saveHighScore(event) {
+  event.preventDefault();
+  let initialInput = document.getElementById("initials").value;
+   let highScoreObj = {
+     name: initialInput,
+     score: countdown,
+   };
+   
+  if (localStorage.getItem("highScore") === null) {
+     localStorage.setItem("highScore", "[]");
+   }
+
+   let oldHighScores = JSON.parse(localStorage.getItem("highScore"));
+   oldHighScores.push(highScoreObj);
+
+  localStorage.setItem("highScore", JSON.stringify(oldHighScores));
+  
+
+
 }
-
-// event listeners //////////////////////////////////////////
-// start button click
 startBtn.addEventListener("click", startQuiz);
+saveScore.addEventListener("click", saveHighScore);
 
-// save high score
-// saveScore.addEventListener('click', saveHighScore);
+  init();
